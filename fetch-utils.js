5c6f38de32396492f6fd3bdb9db7a5b548fd18aa
Spider Forrest <spider@spood.org>
2022-11-30 14:25:03 -0800

    Initial commit

---
+++ fetch-utils.js
@@ -0,0 +1,60 @@
+const SUPABASE_URL = 'https://nwxkvnsiwauieanvbiri.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwMzQzNCwiZXhwIjoxOTUyNTc5NDM0fQ.8XIsU0FANdaNeQnT-DojpTL-GTlTPZ4CYZDEetpFpWc';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export function getUser() {
+    return client.auth.session() && client.auth.session().user;
+}
+
+export async function getFamilies() {
+    // fetch all families and their bunnies
+    // return checkError(response);
+}
+
+export async function deleteBunny(id) {
+    // delete a single bunny using the id argument
+    // return checkError(response);
+}
+
+export async function createBunny(bunny) {
+    // create a bunny using the bunny argument
+    // return checkError(response);
+}
+
+// MARTHA STEWART (PRE-MADE) FUNCTIONS
+
+export async function checkAuth() {
+    const user = getUser();
+
+    if (!user) location.replace('../');
+}
+
+export async function redirectIfLoggedIn() {
+    if (getUser()) {
+        location.replace('./families');
+    }
+}
+
+export async function signupUser(email, password) {
+    const response = await client.auth.signUp({ email, password });
+
+    return response.user;
+}
+
+export async function signInUser(email, password) {
+    const response = await client.auth.signIn({ email, password });
+
+    return response.user;
+}
+
+export async function logout() {
+    await client.auth.signOut();
+
+    return (window.location.href = '../');
+}
+
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
