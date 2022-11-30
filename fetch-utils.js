49a8f564e5435d9215bceefb88997b6dad3eef82
spiderforrest <spider@spood.org>
2022-11-30 15:53:12 -0800

    create functinality

--- fetch-utils.js
+++ fetch-utils.js
@@ -24,7 +24,10 @@ export async function deleteBunny(id) {
 
 export async function createBunny(bunny) {
     // create a bunny using the bunny argument
-    // return checkError(response);
+    const response = await client
+        .from('fuzzy_bunnies')
+        .insert({ name: bunny.name, family_id: bunny.family_id, user_id: client.auth.user().id });
+    return checkError(response);
 }
 
 // MARTHA STEWART (PRE-MADE) FUNCTIONS
